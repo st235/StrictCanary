5e58450860fd925049cb99f5318ad09d22475e08
Alexander Dadukin <hi@st235.xyz>
2023-02-20 11:10:55 +0000

    Implement copy to clipboard feature ui.

--- strictcanary/src/main/java/st235/com/github/strictcanary/utils/StrictCanaryViolationExts.kt
+++ strictcanary/src/main/java/st235/com/github/strictcanary/utils/StrictCanaryViolationExts.kt
@@ -3,8 +3,29 @@ package st235.com.github.strictcanary.utils
 import st235.com.github.strictcanary.data.StrictCanaryViolation
 import st235.com.github.strictcanary.data.StrictCanaryViolationEntry
 
+internal data class ViolationClipBoardDescriptor(
+    val type: String,
+    val entry: String
+)
+
 internal val StrictCanaryViolation.headline: StrictCanaryViolationEntry
     get() {
         return this.violationEntriesStack.getOrNull(myPackageOffset)
             ?: this.violationEntriesStack.first()
+    }
+
+internal val StrictCanaryViolation.clipboardDescriptor: ViolationClipBoardDescriptor
+    get() {
+        val headline = this.headline
+
+        val headlineDescription = StringBuilder()
+            .append(headline.className)
+            .append(".")
+            .append(headline.methodName)
+            .toString()
+
+        return ViolationClipBoardDescriptor(
+            type = this.type.id,
+            entry = headlineDescription
+        )
     }
